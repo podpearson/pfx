560b80039010f25d296eff3c0872fcebdf491452
Richard D Pearson <richard.pearson@well.ox.ac.uk>
2012-12-03 10:02:59 +0000

    bugfix

--- R/reorderSamples.R
+++ R/reorderSamples.R
@@ -12,18 +12,60 @@ reorderSamples <- function(
   parentalIDs,
   sampleIDmappings
 ) {
-  parentalSampleIndexes <- which(sampleIDmappings %in% sampleIDmappings[which(dimnames(GTsInt)[[2]] %in% parentalIDs)])
-  parentalSampleGTsInt <- GTsInt[, parentalSampleIndexes][, order(sampleIDmappings[parentalSampleIndexes])]
-  dimnames(parentalSampleGTsInt)[[2]] <- names(sampleIDmappings)[parentalSampleIndexes][order(sampleIDmappings[parentalSampleIndexes])]
-  progenySampleGTsInt <- GTsInt[, -(parentalSampleIndexes)][, order(sampleIDmappings[-(parentalSampleIndexes)])]
-  dimnames(progenySampleGTsInt)[[2]] <- names(sampleIDmappings)[-(parentalSampleIndexes)][order(sampleIDmappings[-(parentalSampleIndexes)])]
+  parentalSampleIndexes <- which(
+    sampleIDmappings %in% sampleIDmappings[
+      which(dimnames(GTsInt)[[2]] %in% parentalIDs)
+    ]
+  )
+  parentalSampleGTsInt <- GTsInt[
+    ,
+    parentalSampleIndexes
+  ][
+    ,
+    order(sampleIDmappings[parentalSampleIndexes])
+  ]
+  dimnames(parentalSampleGTsInt)[[2]] <- names(sampleIDmappings)[
+    parentalSampleIndexes
+  ][
+    order(sampleIDmappings[parentalSampleIndexes])
+  ]
+  progenySampleGTsInt <- GTsInt[
+    ,
+    -(parentalSampleIndexes)
+  ][
+    ,
+    order(sampleIDmappings[-(parentalSampleIndexes)])
+  ]
+  dimnames(progenySampleGTsInt)[[2]] <- names(sampleIDmappings)[
+    -(parentalSampleIndexes)
+  ][
+    order(sampleIDmappings[-(parentalSampleIndexes)])
+  ]
   reorderedGTsInt <- cbind(parentalSampleGTsInt, progenySampleGTsInt)
   uniqueSampleIDs <- c(
-    sampleIDmappings[parentalSampleIndexes][order(sampleIDmappings[parentalSampleIndexes])],
-    sampleIDmappings[-(parentalSampleIndexes)][order(sampleIDmappings[-(parentalSampleIndexes)])]
+    sampleIDmappings[parentalSampleIndexes][
+      order(sampleIDmappings[parentalSampleIndexes])
+    ],
+    sampleIDmappings[-(parentalSampleIndexes)][
+      order(sampleIDmappings[-(parentalSampleIndexes)])
+    ]
   )
-  linePositions <- which(as.integer(factor(uniqueSampleIDs))!=c(as.integer(factor(uniqueSampleIDs))[-1], NA))
-  browser()
+  linePositions <- which(
+    as.integer(
+      factor(uniqueSampleIDs)
+    )!=c(
+      as.integer(factor(uniqueSampleIDs))[-1],
+      NA
+    )
+  )
   
-  return(list(GTsInt=reorderedGTsInt, linePositions=linePositions, parentalIDs=uniqueSampleIDs))
+  return(
+    list(
+      GTsInt=reorderedGTsInt,
+      linePositions=linePositions,
+      parentalIDs=names(
+        sampleIDmappings[which(dimnames(GTsInt)[[2]] %in% parentalIDs)]
+      )
+    )
+  )
 }
