584084bbe46568dcad69fffaf5317d97ffcfd3cf
podpearson <rpearson@well.ox.ac.uk>
2013-01-10 09:26:53 +0000

    bugfix

--- R/filterGenotypes.R
+++ R/filterGenotypes.R
@@ -23,6 +23,21 @@ filterGenotypes <- function(
     sapply(
       names(genotypeFilters),
       function(filterName) {
+        if(genotypeFilters[[filterName]][["column"]] == "DP" & !("DP" %in% names(geno(vcf))) & ("AD" %in% names(geno(vcf)))) {
+          RefReads <- matrix(
+            sapply(geno(vcf)[["AD"]], function(x) x[1]),
+            ncol=dim(geno(vcf)[["AD"]])[2],
+            dimnames=dimnames(geno(vcf)[["AD"]])
+          )
+          FirstAltReads <- matrix(
+            sapply(geno(vcf)[["AD"]], function(x) x[2]),
+            ncol=dim(geno(vcf)[["AD"]])[2],
+            dimnames=dimnames(geno(vcf)[["AD"]])
+          )
+          AllReads <- RefReads + FirstAltReads
+          AllReads[is.na(AllReads)] <- 0
+          geno(vcf)[["DP"]] <<- AllReads
+        }
         if(genotypeFilters[[filterName]][["operator"]] == "%in%") {
           filteredGenotypes <- geno(vcf)[[genotypeFilters[[filterName]][["column"]]]] %in% genotypeFilters[[filterName]][["value"]]
         } else if(genotypeFilters[[filterName]][["operator"]] == "<=") {
