bdeead75737e7aee080e9a75059773e1bccd2149
podpearson <rpearson@well.ox.ac.uk>
2013-03-26 13:51:51 +0000

    function to load Su10kChip

--- R/compareHb3_Dd2WithUberchip.R
+++ R/compareHb3_Dd2WithUberchip.R
@@ -50,58 +50,56 @@ compareHb3_Dd2WithUberchip <- function(
   )
   browser()
   
-  #  attempt to track down which sample pairs have suspect concordance
-  which(comparisonVsSubjectDiscordanceMatrix>20 & comparisonVsSubjectDiscordanceMatrix<100, arr.ind=TRUE)
-  comparisonVsSubjectDiscordanceMatrix[28,] #JB12 is not particularly good match
-  comparisonVsSubjectDiscordanceMatrix[29,] #DEV matches better to -CW than -C version (which has low coverage)
-  which(comparisonVsSubjectDiscordanceMatrix>10 & comparisonVsSubjectDiscordanceMatrix<=20, arr.ind=TRUE)
-  comparisonVsSubjectDiscordanceMatrix[17,] #KC5 has better match to KC5 (3 discordant SNPs) than to NH11
-  comparisonVsSubjectDiscordanceMatrix[19,] #KB8 has better match to KB8 and KC5 (3 discordant SNPs) than to NH11
+#  #  attempt to track down which sample pairs have suspect concordance
+#  which(comparisonVsSubjectDiscordanceMatrix>20 & comparisonVsSubjectDiscordanceMatrix<100, arr.ind=TRUE)
+#  comparisonVsSubjectDiscordanceMatrix[28,] #JB12 is not particularly good match
+#  comparisonVsSubjectDiscordanceMatrix[29,] #DEV matches better to -CW than -C version (which has low coverage)
+#  which(comparisonVsSubjectDiscordanceMatrix>10 & comparisonVsSubjectDiscordanceMatrix<=20, arr.ind=TRUE)
+#  comparisonVsSubjectDiscordanceMatrix[17,] #KC5 has better match to KC5 (3 discordant SNPs) than to NH11
+#  comparisonVsSubjectDiscordanceMatrix[19,] #KB8 has better match to KB8 and KC5 (3 discordant SNPs) than to NH11
   
   expectedMatches <- rbind(
-    c("XG10", "XG10.PG0109.C.ERR029405"),
-    c("XF12", "XF12_18_05_11.PG0102.CW.ERR045635"),
-    c("XE7", "XE7.PG0106.C.ERR029407"),
-    c("XD8", "XD8_13_05_11.PG0105.CW.ERR045628"),
-    c("XB3", "XB3.PG0093.C.ERR029105"), # 11 discordances
-    c("WF12", "WF12.PG0097.C.ERR027109"),
-    c("WE2", "WE2.PG0085.C.ERR027101"),
-    c("WC4", "WC4.PG0082.C.ERR029093"),
-    c("TF1", "TF1.PG0080.C.ERR027103"), # 19 discordances
-    c("QF5", "QF5.PG0078.C.ERR029092"),
-    c("NIC", "NIC_18_05_11.PG0095.CW.ERR045631"),
-    c("NF10", "NF10.PG0096.C.ERR027108"), # 16 discordances
-    c("LC12", "LC12_5_5_11.PG0110.CW.ERR045641"),
-    c("LA10", "LA10_13_05_11.PG0086.CW.ERR045629"),
-    c("KH7", "KH7.PG0088.C.ERR027111"),
-    c("KC5", "KC5.PG0101.C.ERR029147"),
-    c("KC2", "KC2_5_5_11.PG0090.CW.ERR045640"),
-    c("KB8", "KB8_5_5_11.PG0104.CW.ERR045642"),
-    c("KA6", "KA6.PG0091.C.ERR027117"),
-    c("JON", "JON.PG0107.C.ERR029408"),
-    c("JF6", "JF6.PG0079.CW.ERR045637"),
-    c("JE11", "JE11_18.05_11.PG0100.CW.ERR045630"),
-    c("JC9", "JC9_18_05_11.PG0111.CW.ERR045634"),
-    c("JC3", "JC3.PG0077.CW.ERR045636"),
-    c("JB8", "JB8.PG0087.C.ERR029091"),
-    c("JB12", "JB12.PG0099.C.ERR029146"), # 73 discordances
-#    c("GB4", "GB4_NIH.PG0084.C.ERR027100"),
-    c("DEV", "DEV_18_05_11.PG0081.CW.ERR045633"),
-    c("DAN", "DAN.PG0098.C.ERR027110"),
-    c("AUD", "AUD_5_5_11.PG0112.CW.ERR045639"),
-    c("AL2", "AL2_13_05_11.PG0103.CW.ERR045627")
-#    c("7G8", "X7G8_NIH.PG0083.C.ERR027099")
+    c("CH3_61_1", "CH3_61.PG0033.C.ERR022940"),
+    c("CH3_61_1", "CH3_61.PG0033.Cx.ERR175544"),
+    c("CH3_61_2", "CH3_61.PG0033.C.ERR022940"),
+    c("CH3_61_2", "CH3_61.PG0033.Cx.ERR175544"),
+    c("Dd2_1", "DD2_Ferdig.PG0008.CW.ERR012840"),
+    c("Dd2_2", "DD2_Ferdig.PG0008.CW.ERR012840"),
+    c("Dd2_3", "DD2_Ferdig.PG0008.CW.ERR012840"),
+    c("Dd2_4", "DD2_Ferdig.PG0008.CW.ERR012840"),
+    c("Dd2_5", "DD2_Ferdig.PG0008.CW.ERR012840"),
+    c("HB3_1", "HB3_Ferdig.PG0004.CW.ERR012788"),
+    c("HB3_2", "HB3_Ferdig.PG0004.CW.ERR012788"),
+    c("HB3_3", "HB3_Ferdig.PG0004.CW.ERR012788"),
+    c("HB3_4", "HB3_Ferdig.PG0004.CW.ERR012788"),
+    c("QC23_1", "QC23.PG0045.C.ERR012892"),
+    c("QC23_2", "QC23.PG0045.C.ERR012892"),
+    c("SC05_1", "SC05.PG0019.C.ERR019051"),
+    c("SC05_2", "SC05.PG0019.C.ERR019051"),
+    c("X7C126_1", "X7C126.PG0047.C.ERR015452"),
+    c("X7C126_1", "X7C126.PG0047.C.ERR012891"),
+    c("X7C126_2", "X7C126.PG0047.C.ERR015452"),
+    c("X7C126_2", "X7C126.PG0047.C.ERR012891"),
+    c("X7C126_3", "X7C126.PG0047.C.ERR015452"),
+    c("X7C126_3", "X7C126.PG0047.C.ERR012891"),
+    c("X7C424_1", "X7C424.PG0044.C.ERR019043"),
+    c("X7C424_2", "X7C424.PG0044.C.ERR019043")
   )
-  stem(comparisonVsSubjectDiscordanceMatrix[expectedMatches])
-  median(comparisonVsSubjectDiscordanceMatrix[expectedMatches])
-  1-median(comparisonVsSubjectDiscordanceMatrix[expectedMatches])/dim(subjectMatchesGTs)[1]
-  1-median(comparisonVsSubjectDiscordanceMatrix[expectedMatches])/(dim(subjectMatchesGTs)[1]/2)
+  stem(comparisonVsSubjectDiscordanceMatrixList[[1]][expectedMatches])
+  median(comparisonVsSubjectDiscordanceMatrixList[[1]][expectedMatches])
+  expectedMatches[which(comparisonVsSubjectDiscordanceMatrixList[[1]][expectedMatches] > 1000, arr.ind=TRUE), ]
+#  1-median(comparisonVsSubjectDiscordanceMatrixList[[1]][expectedMatches])/dim(subjectMatchesGTs)[1]
+#  1-median(comparisonVsSubjectDiscordanceMatrixList[[1]][expectedMatches])/(dim(subjectMatchesGTs)[1]/2)
   
 #  double-checking less good matches don't have a better match
-  comparisonVsSubjectDiscordanceMatrix[, "JB12.PG0099.C.ERR029146"]
-  comparisonVsSubjectDiscordanceMatrix[, "XB3.PG0093.C.ERR029105"]
-  comparisonVsSubjectDiscordanceMatrix[, "TF1.PG0080.C.ERR027103"]
-  comparisonVsSubjectDiscordanceMatrix[, "NF10.PG0096.C.ERR027108"]
+  comparisonVsSubjectDiscordanceMatrixList[[1]]["CH3_61_2", ]
+  stem(comparisonVsSubjectDiscordanceMatrixList[[1]]["CH3_61_2", ])
+  min(comparisonVsSubjectDiscordanceMatrixList[[1]]["CH3_61_2", ])
+  which.min(comparisonVsSubjectDiscordanceMatrixList[[1]]["CH3_61_2", ])
+  comparisonVsSubjectDiscordanceMatrixList[[1]]["QC23_2", ]
+  stem(comparisonVsSubjectDiscordanceMatrixList[[1]]["QC23_2", ])
+  min(comparisonVsSubjectDiscordanceMatrixList[[1]]["QC23_2", ])
+  which.min(comparisonVsSubjectDiscordanceMatrixList[[1]]["QC23_2", ])
 
 }
 
---
+++ R/loadSu10kChipAsVcf.R
@@ -0,0 +1,134 @@
+# loadSu10kChipAsVcf.R
+# 
+# Package: pfx
+# Author: Richard D Pearson
+# Email: richard.pearson@well.ox.ac.uk
+#
+###############################################################################
+
+
+loadSu10kChipAsVcf <- function(
+  su10kChipFilename           = "data/Su10kChip/10K_Genotypes_HJiang_80%MinAssayCallRate.txt",
+  su10kChipVcfFilename        = "data/Su10kChip/10K_Genotypes_HJiang_80%MinAssayCallRate.v3.vcf",
+  su10kChipRdaFilename        = "data/Su10kChip/10K_Genotypes_HJiang_80%MinAssayCallRate.v3.vcf.rda",
+  su10kChipV2BedFilename      = "data/Su10kChip/10K_Genotypes_HJiang_80%MinAssayCallRate.bed",
+  su10kChipV3BedFilename      = "data/Su10kChip/10K_Genotypes_HJiang_80%MinAssayCallRate.v3.bed",
+  su10kChipV3UnmappedFilename = "data/Su10kChip/10K_Genotypes_HJiang_80%MinAssayCallRate.v3.unmapped",
+  v2tov3chainFile             = "opt/liftover/2to3.liftOver",
+  liftoverExecutable          = "opt/liftover/liftOver",
+  shouldSaveVcfFile           = TRUE, # Note that due to a bug in VariantAnnotation::.makeVcfGeno it is necessary to add "DUMMY" genotypes data
+  shouldSaveRdaFile           = TRUE,
+  reload                      = FALSE,
+  verbose                     = TRUE
+) {
+  if(reload || !file.exists(su10kChipRdaFilename)) {
+    sampleDetails <- read.table(su10kChipFilename, header=FALSE, sep="\t", as.is=TRUE, nrows=6)
+    sampleManifest <- as.data.frame(t(sampleDetails)[13:51, ], stringsAsFactors=FALSE)
+    names(sampleManifest) <- sampleDetails[, 12]
+    row.names(sampleManifest) <- paste(sampleManifest[["Sample Name "]], sampleManifest[["Experiment Name"]], sep="_")
+    
+    su10kChipV2calls <- read.table(su10kChipFilename, header=TRUE, sep="\t", as.is=TRUE, skip=6)
+    row.names(su10kChipV2calls) <- su10kChipV2calls[["External.Id"]]
+    genotypes <- as.matrix(su10kChipV2calls[, 13:51])
+    dimnames(genotypes) <- list(su10kChipV2calls[["External.Id"]], row.names(sampleManifest))
+    su10kChipV2calls[["RefGenotype"]] <- ifelse(genotypes[, "3D7_(a)4095667-83237"] == genotypes[, "3D7_(a)4095667-82282"] & genotypes[, "3D7_(a)4095667-83237"] %in% c("A", "C", "T", "G"), genotypes[, "3D7_(a)4095667-83237"], NA)
+    goodSNPs <- !is.na(su10kChipV2calls[["RefGenotype"]])
+    su10kChipV2calls <- su10kChipV2calls[goodSNPs, ]
+    genotypes <- genotypes[goodSNPs, as.numeric(sampleManifest[["Experiment Call Rate %"]]) > 0]
+    
+    REFs <- matrix(rep(su10kChipV2calls[["RefGenotype"]], dim(genotypes)[2]), ncol=dim(genotypes)[2], dimnames=dimnames(genotypes))
+    ALTs <- matrix(
+      rep(
+        ifelse(su10kChipV2calls[["Right"]]==su10kChipV2calls[["RefGenotype"]], su10kChipV2calls[["Left"]], su10kChipV2calls[["Right"]]),
+        dim(genotypes)[2]
+      ),
+      ncol=dim(genotypes)[2],
+      dimnames=dimnames(genotypes)
+    )
+    GT <- ifelse(genotypes==REFs, "0", ifelse(genotypes==ALTs, "1", "."))
+  
+    su10kChipV2Bed <- data.frame(
+      Chr = sprintf("Pf3D7_%02d", su10kChipV2calls[["Chrom.Name"]]),
+      Start = su10kChipV2calls[["Chrom.Position"]]-1,
+      End = su10kChipV2calls[["Chrom.Position"]],
+      ID = su10kChipV2calls[["External.Id"]],
+      stringsAsFactors = FALSE
+    )
+    row.names(su10kChipV2Bed) <- su10kChipV2Bed[["ID"]]
+    write.table(su10kChipV2Bed, file=su10kChipV2BedFilename, sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE)
+    system(paste(liftoverExecutable, su10kChipV2BedFilename, v2tov3chainFile, su10kChipV3BedFilename, su10kChipV3UnmappedFilename))  
+    su10kChipV3Bed <- read.table(su10kChipV3BedFilename, header=FALSE, sep="\t", col.names=c("Chr", "Start", "End", "ID"), as.is=TRUE)
+    row.names(su10kChipV3Bed) <- su10kChipV3Bed[["ID"]]
+  
+  #  couple of sanity checks. Firstly are all IDs still the same (they should be), and secondly have any variants changed chromosomes (only a few moved from 8 to 7)
+    identical(su10kChipV2Bed[row.names(su10kChipV3Bed), "ID"], su10kChipV3Bed[["ID"]])
+    table(su10kChipV2Bed[row.names(su10kChipV3Bed), "Chr"], su10kChipV3Bed[["Chr"]])
+    
+    su10kChipRowData  = GRanges(
+      seqnames = su10kChipV3Bed[["Chr"]],
+      ranges   = IRanges(
+        start = su10kChipV3Bed[["End"]],
+        width = 1,
+        names  = su10kChipV3Bed[["ID"]]
+      )
+    )
+      
+    su10kChipVcf <- VCF(
+      rowData = su10kChipRowData,
+      colData = DataFrame(
+        Samples = seq(along=row.names(sampleManifest)),
+        row.names = row.names(sampleManifest)
+      ),
+      exptData = SimpleList(
+        header = VCFHeader(
+          samples = sampleIDs,
+          header  = DataFrameList(
+            META = rbind(
+              DataFrame(Value = "VCFv4.0", row.names="fileformat"),
+              DataFrame(Value = "Su10kChip", row.names="ProjectName"),
+              DataFrame(Value = "7G8_(a)4095667-82251", row.names="PARENT"),
+              DataFrame(Value = "GB4_(a)4095667-82103", row.names="PARENT.1")
+            ),
+  #          FILTER = DataFrame(Descrption=character()),
+            FILTER = DataFrame(Descrption="PASS", row.names="PASS"),
+            FORMAT = rbind(
+              DataFrame(Number = "1", Type="String", Description="Genotype", row.names="GT")
+            ),
+            INFO = rbind(
+              DataFrame(Number = "1", Type = "Integer", Description="Assay.Index", row.names="Assay.Index"),
+              DataFrame(Number = "1", Type = "Integer", Description="Assay.Id", row.names="Assay.Id"),
+              DataFrame(Number = "1", Type = "Float", Description="Assay.Call.Rate..", row.names="Assay.Call.Rate..")
+            )
+          )
+        )
+      ),
+      fixed    = DataFrame(
+        REF = DNAStringSet(REFs[row.names(su10kChipV3Bed), 1]),
+        ALT = DNAStringSet(ALTs[row.names(su10kChipV3Bed), 1]),
+        QUAL = rep(0.0, dim(su10kChipV3Bed)[1]),
+        FILTER = rep("PASS", dim(su10kChipV3Bed)[1])
+      ),
+      info = DataFrame(
+        su10kChipV2calls[row.names(su10kChipV3Bed), c(1, 2, 11), ]
+      ),
+      geno     = SimpleList(
+        GT = GT[row.names(su10kChipV3Bed), ]
+      )
+    )
+  #  genome(su10kChipVcf) <- "Pf"
+    su10kChipVcf <- su10kChipVcf[order(rowData(su10kChipVcf))]
+    if(shouldSaveRdaFile) {
+      save(su10kChipVcf, file=su10kChipRdaFilename)
+    }
+  } else {
+    load(su10kChipRdaFilename)
+  }
+  if(shouldSaveVcfFile) {
+    writeVcf(su10kChipVcf, su10kChipVcfFilename, index=TRUE)
+#    writeVcf(su10kChipVcf, su10kChipVcfFilename)
+  }
+  return(su10kChipVcf)
+}
+
+
+
