b327b92f47d73691d9797a3f5b7ab7dd0381ad75
podpearson <rpearson@well.ox.ac.uk>
2013-04-17 11:29:48 +0100

    write out sensitivity tables

--- R/comparisonsWithArrayData.R
+++ R/comparisonsWithArrayData.R
@@ -10,13 +10,6 @@
 comparisonsWithArrayData <- function(
   reload                      = FALSE
 ) {
-  if(reload || !file.exists("analysis/release/comparisonsWithArrayData/Uberchip_results.rda")) {
-    Uberchip_results <- compareHb3_Dd2WithUberchip()
-    save(Uberchip_results, file="analysis/release/comparisonsWithArrayData/Uberchip_results.rda")
-  } else {
-    load("analysis/release/comparisonsWithArrayData/Uberchip_results.rda")
-  }
-  print(t(sapply(Uberchip_results, function(x) x[["0.5"]][["sensitivityResults"]])))
   if(!file.exists("analysis/release/comparisonsWithArrayData")) {
     dir.create("analysis/release/comparisonsWithArrayData")
   }
@@ -26,11 +19,38 @@ comparisonsWithArrayData <- function(
   } else {
     load("analysis/release/comparisonsWithArrayData/Jiang_results.rda")
   }
+  gc()
   if(reload || !file.exists("analysis/release/comparisonsWithArrayData/Su10k_results.rda")) {
     Su10k_results <- compare7g8_gb4WithSu10kChip2()
     save(Su10k_results, file="analysis/release/comparisonsWithArrayData/Su10k_results.rda")
   } else {
     load("analysis/release/comparisonsWithArrayData/Su10k_results.rda")
   }
-  print(sapply(Su10k_results, function(x) x[[""]]))
+  print(t(sapply(Su10k_results, function(x) x[["sensitivityResults"]])))
+  write.table(
+    t(sapply(Su10k_results, function(x) x[["sensitivityResults"]])),
+    file="analysis/release/comparisonsWithArrayData/Su10k_sensitivity.txt",
+    sep="\t"
+  )
+  gc()
+  if(reload || !file.exists("analysis/release/comparisonsWithArrayData/Uberchip_results.rda")) {
+    Uberchip_results <- compareHb3_Dd2WithUberchip()
+    save(Uberchip_results, file="analysis/release/comparisonsWithArrayData/Uberchip_results.rda")
+  } else {
+    load("analysis/release/comparisonsWithArrayData/Uberchip_results.rda")
+  }
+  print(t(sapply(Uberchip_results, function(x) x[["0.5"]][["sensitivityResults"]])))
+  write.table(
+    t(sapply(Su10k_results, function(x) x[["sensitivityResults"]])),
+    file="analysis/release/comparisonsWithArrayData/Uberchip_sensitivity.txt",
+    sep="\t"
+  )
+  gc()
+  return(
+    list(
+      Jiang_results=Jiang_results,
+      Su10k_results=Su10k_results,
+      Uberchip_results=Uberchip_results
+    )
+  )
 }
